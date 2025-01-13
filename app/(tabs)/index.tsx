b620aa5abafe9805bbe5b4a9ebbff517dd9bc64f
paickle77 <levanan.8b@gmail.com>
2025-01-14 01:53:53 +0700

    lap2CRO

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -28,7 +28,7 @@ export default function HomeScreen() {
             {Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
-              web: 'F12'
+              web: 'F12',
             })}
           </ThemedText>{' '}
           to open developer tools.
@@ -72,3 +72,50 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
 });
+
+const class1 = [ {
+  mssv: 'PS0000',
+  name: 'Nguyen Van A',
+  avgPoint: 8.9,
+  avgTraningPoint: 7,
+  status: 'pass',
+  }, 
+  {
+  mssv: 'PS0001',
+  name: 'Nguyen Van B',
+  avgPoint: 4.9,
+  avgTraningPoint: 10,
+  status: 'pass',
+  }
+  ];
+  const class2 = [ {
+  mssv: 'PS0002',
+  name: 'Nguyen Van C',
+  avgPoint: 4.9,
+  avgTraningPoint: 10,
+  status: 'failed',
+  }, 
+  {
+  mssv: 'PS0003',
+  name: 'Nguyen Van D',  
+  avgPoint: 10,
+  avgTraningPoint: 10,
+  status: 'pass',
+  },
+  {
+  mssv: 'PS0004',
+  name: 'Nguyen Van E',
+  avgPoint: 10,
+  avgTraningPoint: 2,
+  status: 'pass',
+  },
+  ]
+  const allStudent=class1.concat(class2);
+  console.log(allStudent);
+  const quamon= allStudent.filter(student =>student.status =='pass');
+  console.log('danh sách sinh viên qua môn', quamon);
+  const diemso=quamon.sort((a,b) =>a.avgPoint - b.avgPoint);
+  console.log('danh sách sinh viên sắp sếp theo điểm số',diemso);
+  const diemrenluyen=quamon.sort((a,b) =>a.avgTraningPoint - b.avgTraningPoint);
+  console.log('danh sách sinh viên sắp sếp theo điểm rèn luyện',diemrenluyen);
+  
